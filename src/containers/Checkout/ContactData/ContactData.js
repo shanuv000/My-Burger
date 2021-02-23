abcdde5b88ee74babc07e678035a7400ca25f641
VAIBHAV KUMAR <48181718+shanuv000@users.noreply.github.com>
2021-02-24 04:21:32 +0530

    updated

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,6 +3,7 @@ import Button from "../../../components/UI/Button/Button";
 import classes from './ContactData.module.css'
 import axios from "../../../axios-orders";
 import Spinner from "../../../components/UI/Spinner/Spinner";
+import Input from "../../../components/UI/Input/Input";
 
 class ContactData extends Component {
     state = {
@@ -50,10 +51,10 @@ class ContactData extends Component {
 
     render() {
         let form = (<form action="">
-            <input className={classes.Input} type="text" name='name' placeholder='Your Name'/>
-            <input className={classes.Input} type="email" name='email' placeholder='Your email'/>
-            <input className={classes.Input} type="text" name='street' placeholder='Your Street'/>
-            <input className={classes.Input} type="text" name='postal' placeholder='Your Postal Code'/>
+            <Input inputtype='input' type="text" name='name' placeholder='Your Name'/>
+            <Input inputtype='input' type="email" name='email' placeholder='Your email'/>
+            <Input inputtype='input' type="text" name='street' placeholder='Your Street'/>
+            <Input inputtype='input' type="text" name='postal' placeholder='Your Postal Code'/>
             <Button btnType='Success' clicked={this.orderHandler}>Order</Button>
         </form>);
         if (this.state.loading) {
